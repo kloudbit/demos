b7bb288a55ade4455873053696c5c2f2d589bca3
Chris Hickey <chickey@Chriss-MacBook-Air.local>
2016-11-05 02:29:43 -0700

    create user and get access token

--- index.js
+++ index.js
@@ -2,15 +2,30 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
+var unirest = require('unirest');
+var session = require('express-session');
 require('dotenv').config();
 
 app.use(bodyParser.json()); // for parsing application/json
+app.use(session({
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
+  },
+  /*store: store,*/
+  resave: true,
+  saveUninitialized: true
+}));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname+'/views/todo.html'));
 });
 
 app.get('/about', function (req, res) {
+  res.sendFile(path.join(__dirname+'/views/todo.html'));
+});
+
+app.get('/list', function (req, res) {
   res.sendFile(path.join(__dirname+'/views/todo.html'));
 });
 
@@ -25,19 +40,24 @@ app.post('/api/create_user', function (req, res) {
     .end(function (response) {
       console.log(response.body);
 
-      response.body = JSON.parse(response.body);
-      console.log(response.body);
+      unirest.post("http://app.kloudbit.com/1.0/oauth2/token")
+        .headers({'Authorization':'Basic '+process.env.KLOUDBIT_BASIC_TOKEN})
+        .query("username="+req.body.username)
+        .query("password="+req.body.password)
+        .end(function (response){
+          console.log(response.body)
 
-      if(response.body.content){
-        req.session.access_token = response.body.content.access_token;
-        req.session.username = username;
-        console.log(req.session);
-      }
+          if(response.body.access_token){
+            response.body.username = req.body.username;
 
-      res.send(response.body);
+            req.session.access_token = response.body.access_token;
+            req.session.username = req.body.username;
+            console.log(req.session);
+          }
+
+          res.send(response.body);
+        });
     });
-
-  res.end();
 });
 
 app.listen(3002, function () {
